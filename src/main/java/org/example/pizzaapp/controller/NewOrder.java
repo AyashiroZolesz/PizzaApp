7302456d28f018b24774d2a550ff134b8188c40f
acszoltan14@stud.uni-obuda.hu <acszoltan14@stud.uni-obuda.hu>
2024-11-12 13:56:18 +0100

    Configured NewOrder servlet and updated index.html

--- src/main/java/org/example/pizzaapp/controller/NewOrder.java
+++ src/main/java/org/example/pizzaapp/controller/NewOrder.java
@@ -1,4 +1,22 @@
 package org.example.pizzaapp.controller;
+import org.example.pizzaapp.model.Pizza;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 
 public class NewOrder {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Create the pizza and add it to the session scope
+        int size = Integer.parseInt(request.getParameter("size"));
+        Pizza newpizza = new Pizza(size);
+        request.getSession().setAttribute("pizza", newpizza);
+
+        // Forward the request to the toppings manager (in case available toppings list is not created yet)
+        request.getRequestDispatcher("ManageToppings").forward(request, response);
+    }
+
 }
